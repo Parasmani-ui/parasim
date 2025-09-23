22919f724235bc1bc8fed66229a3963018bb8e02
Vikash Lanjhikar <vikashlanjhi@bhu.ac.in>
2025-09-23 19:24:40 +0530

    appf

---
+++ app/webpack.config.js
@@ -0,0 +1,38 @@
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/build',
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({})]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{ loader: 'babel-loader' }],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(svg|png|jpeg|jpg|gif)$/,
+        use: [{ loader: "file-loader"}],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader"}, { loader: "css-loader"}],
+      }
+    ],
+  }
+};
